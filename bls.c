f4149c2cb2ac91cf33e7e877f044bbbb7d6e33b7
malloc-nbytes <zdhdev@yahoo.com>
2025-06-19 20:43:23 -0500

    add bls

---
+++ bls.c
@@ -0,0 +1,32 @@
+#include <forge/forge.h>
+
+char *getname(void) { return "bls"; }
+char *getver(void) { return "1.0.0"; }
+char *getdesc(void) { return "An alternative to `ls`"; }
+char *download(void) {
+        return git_clone("malloc-nbytes", "bls");
+}
+void build(void) {
+        cd("src");
+        cmd("make");
+}
+void install(void) {
+        cd("src");
+        cmd("make install");
+        cmd("make clean");
+}
+void uninstall(void) {
+        cmd("rm /usr/local/bin/bls");
+}
+
+FORGE_GLOBAL pkg package = {
+        .name = getname,
+        .ver = getver,
+        .desc = getdesc,
+        .deps = NULL,
+        .download = download,
+        .build = build,
+        .install = install,
+        .uninstall = uninstall,
+        .update = forge_pkg_git_update,
+};
