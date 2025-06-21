53e20bb8015bc4f8454a6479abf515d12c782fc3
malloc-nbytes <zdhdev@yahoo.com>
2025-06-21 16:37:47 -0500

    add gdb

---
+++ gdb.c
@@ -0,0 +1,34 @@
+#include <forge/forge.h>
+
+char *deps[] = {NULL}; // Must be NULL terminated
+
+char *getname(void) { return "GNU/gdb"; }
+char *getver(void) { return "16.2.0"; }
+char *getdesc(void) { return "The GNU Project Debugger"; }
+char **getdeps(void) { return deps; }
+char *download(void) {
+        cmd("git clone https://sourceware.org/git/binutils-gdb.git --depth=1");
+        return "binutils-gdb";
+}
+void build(void) {
+        cmd("./configure");
+        cmd("./make -j12");
+}
+void install(void) {
+        cmd("make install");
+}
+void uninstall(void) {
+        cmd("make uninstall");
+}
+
+FORGE_GLOBAL pkg package = {
+        .name = getname,
+        .ver = getver,
+        .desc = getdesc,
+        .deps = NULL,
+        .download = download,
+        .build = build,
+        .install = install,
+        .uninstall = uninstall,
+        .update = forge_pkg_git_update,
+};
