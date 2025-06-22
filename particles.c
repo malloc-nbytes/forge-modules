7a4e1b329f11e86eec1584f3150a75dfb4400312
malloc-nbytes <zdhdev@yahoo.com>
2025-06-21 23:46:17 -0500

    add particles

---
+++ particles.c
@@ -0,0 +1,32 @@
+#include <forge/forge.h>
+
+char *deps[] = {"raysan5/raylib", NULL}; // Must be NULL terminated
+
+char *getname(void) { return "malloc-nbytes/particles"; }
+char *getver(void) { return "1.0.0"; }
+char *getdesc(void) { return "Just a fun graphics application"; }
+char **getdeps(void) { return deps; }
+char *download(void) {
+        return git_clone("malloc-nbytes", "particles");
+}
+void build(void) {
+	cmd("./build.sh");
+}
+void install(void) {
+	cmd("cp ./particles /usr/local/bin");
+}
+void uninstall(void) {
+	cmd("rm /usr/local/bin/particles");
+}
+
+FORGE_GLOBAL pkg package = {
+        .name = getname,
+        .ver = getver,
+        .desc = getdesc,
+        .deps = getdeps,
+        .download = download,
+        .build = build,
+        .install = install,
+        .uninstall = uninstall,
+        .update = forge_pkg_git_update, // or define your own if not using git
+};
