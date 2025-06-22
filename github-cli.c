095c48a80760266debab892caf6c5262ed1b248a
malloc-nbytes <zdhdev@yahoo.com>
2025-06-21 22:53:26 -0500

    add gh

---
+++ github-cli.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <forge/forge.h>
+
+char *deps[] = {/*"golang"*/NULL}; // Must be NULL terminated
+
+char *getname(void) { return "Github/github-cli"; }
+char *getver(void) { return "2.68.1"; }
+char *getdesc(void) { return "GitHub’s official command line tool "; }
+char **getdeps(void) { return deps; }
+char *download(void) {
+        return git_clone("cli", "cli");
+}
+void build(void) {
+        cmd("make");
+}
+void install(void) {
+        cmd("make install");
+}
+void uninstall(void) {
+        cmd("make uninstall");
+}
+
+FORGE_GLOBAL pkg package = {
+        .name = getname,
+        .ver = getver,
+        .desc = getdesc,
+        .deps = NULL,
+        .download = download,
+        .build = build,
+        .install = install,
+        .uninstall = uninstall,
+        .update = forge_pkg_git_update,
+};
