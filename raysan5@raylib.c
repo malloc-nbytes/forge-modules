e6a108219b6b5e7870254caa22bfc14fc0404589
malloc-nbytes <zdhdev@yahoo.com>
2025-07-01 12:37:49 -0500

    update raylib

--- raysan5@raylib.c
+++ raysan5@raylib.c
@@ -7,23 +7,23 @@ char *getver(void) { return "5.5.0"; }
 char *getdesc(void) { return "A simple and easy-to-use library to enjoy videogames programming"; }
 char **getdeps(void) { return deps; }
 char *download(void) {
-        cmd("git clone https://github.com/raysan5/raylib.git");
+        cmd("git clone https://github.com/raysan5/raylib.git --depth=1");
         return "raylib";
 }
 void build(void) {
-        cmd("mkdir build");
+        mkdirp("build");
         cd("build");
         cmd("cmake ..");
-        cmd("make -j12");
+        make(NULL);
 }
 void install(void) {
         cd("build");
-        cmd("make install");
+        make("install");
         cmd("ldconfig");
 }
 void uninstall(void) {
         cd("build");
-        cmd("make uninstall");
+        make("uninstall");
 }
 
 FORGE_GLOBAL pkg package = {
