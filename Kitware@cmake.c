6c0fd48b6b5fa2540d0fe444983216e0146fd61d
malloc-nbytes <zdhdev@yahoo.com>
2025-07-12 21:21:05 -0500

    update cmake

--- Kitware@cmake.c
+++ Kitware@cmake.c
@@ -4,11 +4,11 @@ char *getname(void) { return "Kitware@cmake"; }
 char *getver(void) { return "3.31.7"; }
 char *getdesc(void) { return "Software build system"; }
 char *download(void) {
-        cmd("git clone https://gitlab.kitware.com/cmake/cmake/-/tree/release?ref_type=heads");
+        cmd("git clone https://gitlab.kitware.com/cmake/cmake.git");
         return "cmake";
 }
 void build(void) {
-        cmd("/bin/sh bootstraph.sh --parallel=" FORGE_PREFERRED_MAKEFILE_JFLAGS);
+        cmd("/bin/sh bootstrap --parallel=" FORGE_PREFERRED_MAKEFILE_JFLAGS);
         make(NULL);
 }
 void install(void) {
