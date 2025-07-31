a3df6fabdd642497b01bb7c7f03ca14e969f3eaa
malloc-nbytes <zdhdev@yahoo.com>
2025-07-31 17:31:47 -0500

    add vile

---
+++ malloc-nbytes@vile.c
@@ -0,0 +1,30 @@
+#include <forge/forge.h>
+
+char *getname(void) { return "malloc-nbytes@vile"; }
+char *getver(void) { return "1.0.0"; }
+char *getdesc(void) { return "Manipulate C files easily"; }
+char *download(void) {
+        return git_clone("malloc-nbytes", "vile");
+}
+void build(void) {
+        cmd("/bin/sh build.sh");
+}
+void install(void) {
+        cmd("cp vile " FORGE_PREFERRED_INSTALL_PREFIX "/bin");
+}
+void uninstall(void) {
+        cmd("rm " FORGE_PREFERRED_INSTALL_PREFIX "/bin/vile");
+}
+
+FORGE_GLOBAL pkg package = {
+        .name = getname,
+        .ver = getver,
+        .desc = getdesc,
+        .deps = NULL,
+        .download = download,
+        .build = build,
+        .install = install,
+        .uninstall = uninstall,
+        .update = forge_pkg_git_update,
+        .get_changes = forge_pkg_git_pull,
+};
