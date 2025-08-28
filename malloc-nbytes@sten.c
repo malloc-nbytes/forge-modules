9f46e3ce2f49c1fd29e7096c412596d52e0b639b
malloc-nbytes <zdhdev@yahoo.com>
2025-08-28 15:23:12 -0500

    update sten

--- malloc-nbytes@sten.c
+++ malloc-nbytes@sten.c
@@ -1,6 +1,6 @@
 #include <forge/forge.h>
 
-char *deps[] = {NULL}; // Must be NULL terminated
+char *deps[] = {"malloc-nbytes@earl", NULL}; // Must be NULL terminated
 
 char *getname(void)  { return "malloc-nbytes@sten"; }
 char *getver(void)   { return "WIP"; }
@@ -8,9 +8,9 @@ char *getdesc(void)  { return "A WIP text editor"; }
 char *getweb(void)   { return "https://www.github.com/malloc-nbytes/sten.git/"; }
 char *download(void) { return git_clone("malloc-nbytes", "sten"); }
 
-int build(void)     { return cmd("/bin/sh build.sh"); }
-int install(void)   { return cmd("cp ./sten " FORGE_PREFERRED_INSTALL_PREFIX "/bin"); }
-int uninstall(void) { return cmd("rm " FORGE_PREFERRED_INSTALL_PREFIX "/bin/sten"); }
+int build(void)      { return cmd("earl build.rl"); }
+int install(void)    { return cmd("cp ./sten " FORGE_PREFERRED_INSTALL_PREFIX "/bin"); }
+int uninstall(void)  { return cmd("rm " FORGE_PREFERRED_INSTALL_PREFIX "/bin/sten"); }
 
 FORGE_GLOBAL pkg package = {
         .name = getname,
