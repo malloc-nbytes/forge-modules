68f46cc8336c577d6f54a160b733c347ca87cafe
malloc-nbytes <zdhdev@yahoo.com>
2025-12-27 11:32:25 -0600

    update ampire

--- malloc-nbytes@ampire.c
+++ malloc-nbytes@ampire.c
@@ -1,32 +1,37 @@
 #include <forge/forge.h>
 
+char *msgs[] = {
+        "Remember to run `ldconfig`!",
+        NULL,
+};
+
+char **getmsgs(void) { return msgs; }
 char *getname(void)  { return "malloc-nbytes@ampire"; }
 char *getver(void)   { return "rolling"; }
 char *getdesc(void)  { return "A music player in the terminal"; }
 char *getweb(void)   { return "https://www.github.com/malloc-nbytes/ampire.git/"; }
 
-char *download(void) {
-        //CMD("git clone https://www.github.com/malloc-nbytes/ampire.git/ --recursive", return NULL);
+char *
+download(void)
+{
         CMD("git clone https://www.github.com/malloc-nbytes/ampire.git/ --recurse-submodules --shallow-submodules --depth=1", return NULL);
         return "ampire";
 }
 
-int build(void) {
-        mkdirp("build");
-        cd("build");
+int
+build(void)
+{
+        if (!mkdirp("build")) return 0;
+        CD("build", return 0);
         CMD("cmake -S .. -B .", return 0);
         return make(NULL);
 }
 
-int install(void) {
-        cd("build");
-        if (!make("install")) return 0;
-        return cmd("ldconfig");
-}
-
-int uninstall(void) {
-        cd("build");
-        return make("uninstall");
+int
+install(void)
+{
+        CD("build", return 0);
+        return make("install");
 }
 
 FORGE_GLOBAL pkg package = {
@@ -35,13 +40,13 @@ FORGE_GLOBAL pkg package = {
         .desc            = getdesc,
         .web             = getweb,
         .deps            = NULL,
-        .msgs            = NULL,
+        .msgs            = getmsgs,
         .suggested       = NULL,
         .rebuild         = NULL,
         .download        = download,
         .build           = build,
         .install         = install,
-        .uninstall       = uninstall,
+        .uninstall       = NULL,
         .update          = forge_pkg_git_update,
         .get_changes     = forge_pkg_git_pull,
 };
